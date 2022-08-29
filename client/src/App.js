3eee3f4be731e97871b80ec8d67b9cb63815344b
kj312000 <kaustubhjadhav36@gmail.com>
2022-08-29 15:37:44 +0530

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import {Routes , Route , useNavigate} from 'react-router-dom'
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    <Routes>
+      <Route path='login' element={<Login/>}/>
+      <Route path='/*' element={<Home/>}/>
+    </Routes>
+  ); 
 }
 
 export default App;
